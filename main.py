73a98568d7a192089bd3ce202fc4106e4b269c4f
VillegasMich <manuel.villegas.michel@gmail.com>
2022-08-07 15:46:13 -0500

    Primera entrega

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def main():
+    file_name = "calles_de_medellin_con_acoso.csv"
+    medellin = pd.read_csv(file_name, sep=';')
+
+    medellin_full = medellin.fillna({"harassmentRisk":medellin.harassmentRisk.mean()}).fillna({"name":"---"})
+    print(f"{file_name} was now saved on medellin_full.csv without empty values")
+    medellin_full.to_csv("medellin_full.csv")
+
+main()
